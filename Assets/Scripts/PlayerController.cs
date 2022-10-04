0cbea972af6ed0bb6a424fff604ba116912a5fab
SujayAmberkar <sujayamberkar@gmail.com>
2022-10-04 20:33:56 +0530

    Player synch on both Device

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,17 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Photon.Pun;
 
 public class PlayerController : MonoBehaviour
 {
     public Vector2 moveVal;
     public float speed = 10f;
     Rigidbody2D rbd;
+    PhotonView view;
 
     // Start is called before the first frame update
     void Start()
     {
         rbd = GetComponent<Rigidbody2D>();
+        view = GetComponent<PhotonView>();
     }
 
     // Update is called once per frame
@@ -25,9 +28,12 @@ public class PlayerController : MonoBehaviour
         moveVal = value.Get<Vector2>();
     }
     void Movements(){
-        Vector2 playerVelocity = new Vector2(moveVal.x*speed,moveVal.y*speed);
-        rbd.velocity = playerVelocity;
-        bool playerHasHorizontalSpeed = Mathf.Abs(rbd.velocity.x) > 0;
+        if(view.IsMine){
+            Vector2 playerVelocity = new Vector2(moveVal.x*speed,moveVal.y*speed);
+            rbd.velocity = playerVelocity;
+            bool playerHasHorizontalSpeed = Mathf.Abs(rbd.velocity.x) > 0;
+        }
+        
         
     }
 }
